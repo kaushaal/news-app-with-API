1ce0e50cbf8ca185c06a2018f1fd7dce1e88fdff
kaushaal <kaushaloza1997@gmail.com>
2024-03-18 17:36:29 +0530

    search feature completed

--- script.js
+++ script.js
@@ -1,90 +1,86 @@
-const apiKey = "87158d7cd8d646b681696908fee34aa6";
+const apiKey = "c77e66952b594d019c0f30c90dc72d5b";
+const blogContainer = document.getElementById("blogContainer");
+const searchField = document.getElementById("searchField");
+const searchButton = document.getElementById("searchButton");
 
-// const blogContainer = document.getElementById("blog-container");
-
-// async function fetchRandom() {
-//   try {
-//     // const apiURL = `https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apikey=${apiKey}`;
-//     const apiURL = `https://newsapi.org/v2/top-headlines?sources=techcrunch&pageSize=5&apiKey=${apiKey}`;
-
-//     const response = await fetch(apiURL);
-//     const data = await response.json();
-//     console.log("Successful");
-//     return data.articles;
-//   } catch (error) {
-//     console.error("Error while fetching a random news", error);
-//     return [];
-//   }
-// }
-
-// function displayBlogs(articles) {
-//   blogContainer.innerHTML = "";
-//   articles.forEach((article) => {
-//     const blogCard = document.createElement("div");
-//     blogCard.classList.add("blog-card");
-//     const img = document.createElement("img");
-//     img.src = article.urlToImage;
-//     img.alt = article.title;
-//     const title = document.createElement("h2");
-//     title.textContent = article.title;
-//     const description = document.createElement("p");
-//     description.textContent = article.description;
-
-//     blogCard.appendChild(img);
-//     blogCard.appendChild(title);
-//     blogCard.appendChild(description);
-//     blogContainer.appendChild(blogCard);
-//   });
-// }
-
-// (async () => {
-//   try {
-//     const articles = await fetchRandom();
-//     displayBlogs(articles);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// })();
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Fetch data and update DOM
-  fetchRandom()
-    .then(displayBlogs)
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-    });
-});
-
-async function fetchRandom() {
+async function fetchRandomNews() {
   try {
-    const apiURL = `https://newsapi.org/v2/top-headlines?sources=techcrunch&pageSize=4&apiKey=${apiKey}`;
+    const apiURL = `https://newsapi.org/v2/top-headlines?sources=techcrunch&pageSize=20&apiKey=${apiKey}`;
     const response = await fetch(apiURL);
     const data = await response.json();
     return data.articles;
   } catch (error) {
-    console.error("Error while fetching data", error);
-    throw error;
+    console.error("Error fetching API", error);
+    return [];
+  }
+}
+
+searchButton.addEventListener("click", async () => {
+  const query = searchField.value.trim();
+  console.log(query);
+  if (query !== "") {
+    try {
+      const articles = await fetchQuery(query);
+      displayBlogs(articles);
+    } catch (error) {
+      console.error("Error fetching query", error);
+    }
+  }
+});
+
+async function fetchQuery(query) {
+  try {
+    const apiURL = `https://newsapi.org/v2/everything?q=${query}&pageSize=50&apiKey=${apiKey}`;
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    return data.articles;
+  } catch (error) {
+    console.error("Error fetching API", error);
+    return [];
   }
 }
 
 function displayBlogs(articles) {
-  const blogContainer = document.getElementById("blog-container");
   blogContainer.innerHTML = "";
 
   articles.forEach((article) => {
     const blogCard = document.createElement("div");
-    blogCard.classList.add("blog-card");
+    blogCard.classList.add("blogCard");
+
     const img = document.createElement("img");
     img.src = article.urlToImage;
     img.alt = article.title;
+
     const title = document.createElement("h2");
-    title.textContent = article.title;
+    const truncatedTitle =
+      article.title.length > 40
+        ? article.title.slice(0, 40) + "... "
+        : article.title;
+    title.textContent = truncatedTitle;
+
     const description = document.createElement("p");
-    description.textContent = article.description;
+    const truncatedDesc =
+      article.description.length > 100
+        ? article.description.slice(0, 100) + "... "
+        : article.description;
+    description.textContent = truncatedDesc;
 
     blogCard.appendChild(img);
     blogCard.appendChild(title);
     blogCard.appendChild(description);
+    blogCard.addEventListener("click", () => {
+      window.open(article.url);
+      ("_blank");
+    });
     blogContainer.appendChild(blogCard);
   });
 }
+
+(async () => {
+  try {
+    const articles = await fetchRandomNews();
+    displayBlogs(articles);
+  } catch (error) {
+    console.error("Error fetching news", error);
+  }
+})();
